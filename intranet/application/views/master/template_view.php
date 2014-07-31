dc7d8a5e58e413d877f5af4368054961be1fd175
developerluiggi <luiggichirinos_p@outlook.com>
2014-07-30 19:00:55 -0500

    liempieza ino

--- intranet/application/views/master/template_view.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->load->view('master/header_view',$breadcrumbs);
-$this->load->view($main_content);
-$this->load->view('master/footer_view');
-?>
--- portal/application/views/master/template_view.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->load->view('master/header_view');
-$this->load->view($main_content);
-$this->load->view('master/footer_view');
-?>
