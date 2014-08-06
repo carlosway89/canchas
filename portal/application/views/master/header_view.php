daf340558b374b1ce9fa9a84c71b737b6b73ce3a
Carlosway89 <carlosway89@gmail.com>
2014-08-05 19:48:50 -0500

    robot y backup

--- portal/application/views/master/header_view.php
+++ portal/application/views/master/header_view.php
@@ -99,7 +99,7 @@
                             <!-- Newsletter -->
                             <div class="col-xs-12 col-lg-4 col-md-5 col-sm-5">
 
-                                <form id="newsletter" action="http://solocanchas.com/portal/acceso/autentication" method="POST">
+                                <form id="newsletter" action="http://localhost/canchas/portal/acceso/autentication" method="POST">
 
                                     <h5 id="titulo-acceder">
                                         <i class="icons icon-laptop"></i>
